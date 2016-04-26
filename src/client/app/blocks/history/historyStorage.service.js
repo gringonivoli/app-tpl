1986b31c17672d07724431a673878ce715af8473
Maxi Nivoli <m_nivoli@hotmail.com>
2016-04-26 19:37:40 -0300

    Recuerdo de valor Y para usar a la vuelta de una modal.

--- src/client/app/blocks/history/historyStorage.service.js
+++ src/client/app/blocks/history/historyStorage.service.js
@@ -1,80 +1,87 @@
-(function() {
-'use strict';
+(function () {
+    'use strict';
 
-angular
-    .module('blocks.history')
-    .factory('historyStorage', historyStorage);
+    angular
+        .module('blocks.history')
+        .factory('historyStorage', historyStorage);
 
-historyStorage.$inject = ['store'];
-function historyStorage(store) {
+    historyStorage.$inject = ['store', '$window', '$timeout'];
+    function historyStorage(store, $window, $timeout) {
 
-    var id = 'historyStore';
+        var id = 'historyStore';
 
-    var service = {
+        var service = {
             set: set,
             get: get,
             remove: remove
         };
 
-    init();
+        init();
 
-    return service;
+        return service;
 
-    ////////////////
+        ////////////////
 
-    /**
-     * init
-     *
-     * Inicializa el storage vacio.
-     *
-     */
-    function init() {
+        /**
+         * init
+         *
+         * Inicializa el storage vacio.
+         *
+         */
+        function init() {
             store.set(id, {});
         }
 
-    /**
-     * set
-     *
-     * Setea una nueva propidad
-     * al objeto de storage y lo
-     * hace persistente.
-     *
-     * @param {String} name
-     * @param {Object} val
-     * @returns {*}
-     */
-    function set(name, val) {
+        /**
+         * set
+         *
+         * Setea una nueva propidad
+         * al objeto de storage y lo
+         * hace persistente.
+         *
+         * @param {String} name
+         * @param {Object} val
+         * @returns {*}
+         */
+        function set(name, val) {
+            val.scrollY = $window.pageYOffset;
             var obj = store.get(id);
             obj[name] = val;
             return store.set(id, obj);
         }
 
-    /**
-     * get
-     *
-     * Retorna una propiedad del
-     * objeto storage.
-     *
-     * @param {String} name
-     * @returns {*}
-     */
-    function get(name) {
+        /**
+         * get
+         *
+         * Retorna una propiedad del
+         * objeto storage.
+         *
+         * @param {String} name
+         * @returns {*}
+         */
+        function get(name) {
             var obj = store.get(id);
-            return obj[name];
+            obj = obj[name];
+            if (obj && obj.hasOwnProperty('scrollY')) {
+                $timeout(function () {
+                    $window.scrollTo(0, obj.scrollY);
+                }, 300);
+            }
+            return obj;
         }
 
-    /**
-     * remove
-     *
-     * Elimina una propiedad del
-     * objeto storage y lo persiste.
-     *
-     * @param {String} name
-     */
-    function remove(name) {
+        /**
+         * remove
+         *
+         * Elimina una propiedad del
+         * objeto storage y lo persiste.
+         *
+         * @param {String} name
+         */
+        function remove(name) {
             var obj = store.get(id);
             delete obj[name];
             store.set(id, obj);
         }
-}
+    }
 })();
