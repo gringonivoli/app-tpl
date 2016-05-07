8bd2daa3426d060e21cbe78d1769071368c4330a
Maxi Nivoli <m_nivoli@hotmail.com>
2016-05-07 16:14:27 -0300

    Fix bug exception.service.

--- src/client/app/blocks/exception/exception.service.js
+++ src/client/app/blocks/exception/exception.service.js
@@ -14,15 +14,7 @@
 
         function catcher(message) {
             return function(e) {
-                var thrownDescription;
-                var newMessage;
-                if (e.data && e.data.description) {
-                    thrownDescription = '\n' + e.data.description;
-                    newMessage = message + thrownDescription;
-                }
-                e.data.description = newMessage;
-                logger.error(newMessage);
-                return $q.reject(e);
+                logger.error(message, e);
             };
         }
     }
